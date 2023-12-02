2b54945350cf20619de185dfa325c563cb596245
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-02 15:54:13 +0100

    almost done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // function putNav(id){
     const nav = document.querySelector('nav');
-    nav.innerHTML = '<div class="left"><ul><li><a href="index.htm1">home</a></li><li><a href="work.html">work</a></li><li><a href="about.html">about</a></li></ul></div><div class="center"><h5>BUILT BY</h5><img src="images/logo-plain@2x.png" alt=""></div><div class="right"><ul><li><a href="feed.html">feeds</a></li><li><a href="job.html">jobs</a></li><li><a href="contact.html">contact</a></li></ul></div>';
+    nav.innerHTML = '<div class="left"><ul><li><a href="index.html">home</a></li><li><a href="work.html">work</a></li><li><a href="about.html">about</a></li></ul></div><div class="center"><h5>BUILT BY</h5><img src="images/logo-plain@2x.png" alt=""></div><div class="right"><ul><li><a href="feed.html">feeds</a></li><li><a href="job.html">jobs</a></li><li><a href="contact.html">contact</a></li></ul></div>';
 // }
 
