51d7da4ef4ffebf0fb881d247dc5ee3943882c84
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-03 21:29:06 +0100

    footer created

--- nav.js
+++ nav.js
@@ -3,5 +3,5 @@
     nav.innerHTML = '<div class="left"><ul><li><a href="index.html">home</a></li><li><a href="work.html">work</a></li><li><a href="about.html">about</a></li></ul></div><div class="center"><h5>BUILT BY</h5><img src="images/logo-plain@2x.png" alt=""></div><div class="right"><ul><li><a href="feed.html">feeds</a></li><li><a href="job.html">jobs</a></li><li><a href="contact.html">contact</a></li></ul></div>';
 // }
 
-let radio = document.getElementsByClassName('radio');
+const foot = document.querySelector('footer');
 
