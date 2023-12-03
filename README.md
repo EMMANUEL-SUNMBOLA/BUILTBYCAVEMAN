91ccf4cfb88b8b8404c6f873730fb81c3f3236fe
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-03 21:36:01 +0100

    footer universal

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 <h1>#BUILTBYCAVEMAN</h1>
 <h4>clone of <a href="https://builtbybuffalo.com/">BUILTBYBUFFALO</a> website</h4>
 <p>To ensure that only one radio input can be picked at a time give all the inputs the same name </p>
+<p>the nav and footer are generated from <a href="nav.js">nav.js</a></p>
 ---------------------------------<<+>>------------------------------------------
 
--- nav.js
+++ nav.js
@@ -4,4 +4,5 @@
 // }
 
 const foot = document.querySelector('footer');
+foot.innerHTML = '<h4>we are proud to be reccomended by these fine folks:</h4><div class="partner"><p><i class="fa-brands fa-shopify">shopify</i> partners</p><p><i class="fa-brands fa-mailchimp">mailchimp</i><br>partners</p></div><h4>Copyright &:copy 2006-2089 <p><a href="https://builtbycaveman.netlify.app/">Built by Caveman</a></p> limited all rights reserved</h4><h4>registered in nigeria compay no.86848 </h4><h4><p><a href="#">Read the boring legal stuff</a></p></h4>';
 
