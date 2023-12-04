37ac8a62e5b6cbbe3feb2da6a6c2d1c5f33a00f8
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-04 23:33:07 +0100

    cdn

--- nav.js
+++ nav.js
@@ -4,5 +4,18 @@
 // }
 
 const foot = document.querySelector('footer');
-foot.innerHTML = '<h4>we are proud to be reccomended by these fine folks:</h4><div class="partner"><p><i class="fa-brands fa-shopify">shopify</i> partners</p><p><i class="fa-brands fa-mailchimp">mailchimp</i><br>partners</p></div><h4>Copyright &:copy 2006-2089 <p><a href="https://builtbycaveman.netlify.app/">Built by Caveman</a></p> limited all rights reserved</h4><h4>registered in nigeria compay no.86848 </h4><h4><p><a href="#">Read the boring legal stuff</a></p></h4>';
+foot.innerHTML = '<hr><h4>we are proud to be reccomended by these fine folks:</h4><div class="partner"><p><i class="fa-brands fa-shopify">shopify</i> partners</p><p><i class="fa-brands fa-mailchimp">mailchimp</i><br>partners</p></div><h4>Copyright &:copy 2006-2089 <p><a href="https://builtbycaveman.netlify.app/">Built by Caveman</a></p> limited all rights reserved</h4><h4>registered in nigeria compay no.86848 </h4><h4><p><a href="#">Read the boring legal stuff</a></p></h4>';
 
+const conn = document.getElementById('conn');
+conn.innerHTML = '<a href="#"><div class="sub"><div class="icon" title="proposal planner"><i class="fa-solid fa-map"></i></div><h1>project planner</h1><p>Interested in working with us? Launch a proposal planner to get started</p><button>launch it</button></div></a><div id="sub"><div class="icon" title="Connect"><i class="fa-solid fa-message"></i></div><h1>connect</h1><p><a href="#" id="buffalink">hello@builtbybuffalo.com</a></p></div><a href="#"><div class="sub"><div class="icon" title="Get in touch"><i class="fa-solid fa-calendar"></i></div><h1>calendly</h1><p>Schedule a time to talk to us and get things moving.</p><button>book in</button></div></a>';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const cdnFontAwe = document.createElement('link');
+    cdnFontAwe.rel = "stylesheet";
+    cdnFontAwe.href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css";
+    cdnFontAwe.integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==";
+    cdnFontAwe.crossorigin="anonymous";
+    cdnFontAwe.referrerpolicy="no-referrer";
+    document.querySelector('head').appendChild(cdnFontAwe);
+})
