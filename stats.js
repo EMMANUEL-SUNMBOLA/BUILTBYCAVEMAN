36eb340cd6141556693e963ebc03ce9edc8904ce
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-05 14:23:49 +0100

    progress bar works

--- stats.js
+++ stats.js
@@ -1,22 +1,28 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+  let outDiv = ['out1', 'out2', 'out3', 'out4', 'out5'];
+  let inDiv = ['in1', 'in2', 'in3', 'in4', 'in5'];
+  let percents = ['60', '80', '57', '83', '90']
+  for( let i = 0; i <  5; i++){
+      let CircularBar = document.querySelector("." + inDiv[i]);
 
-    let CircularBar = document.querySelector(".inner-circle");
-    let PercentValue = document.querySelector(".percent");
-    
-    let InitialValue = 0;
-    let finaleValue = 60;
-    let speed = 10;
-    
-    let timer = setInterval(() => {
-      InitialValue += 1;
-    
-      CircularBar.style.background = `conic-gradient(#16a6b6 ${InitialValue/100 * 360}deg, #fff 0deg)`;
-      PercentValue.innerHTML = InitialValue+"%";
-    
-      if(InitialValue >= finaleValue){
-        clearInterval(timer);
-      }
-    }, speed)
+      let PercentValue = document.querySelector(".percent");
+      
+      let InitialValue = 0;
+      let finaleValue = percents[i];
+      let speed = 30;
+      
+      let timer = setInterval(() => {
+        InitialValue += 1;
+      
+        CircularBar.style.background = `conic-gradient(#16a6b6 ${InitialValue/100 * 360}deg, #fff 0deg)`;
+        PercentValue.innerHTML = InitialValue+"%";
+      
+        if(InitialValue >= finaleValue){
+          clearInterval(timer);
+        }
+      }, speed)
+
+    }
 
 
 })
