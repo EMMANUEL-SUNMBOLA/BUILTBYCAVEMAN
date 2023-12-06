7152960e971feea8e8f6594d41a2adb2e6a3beea
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-06 10:12:54 +0100

    progress bar complete

--- stats.js
+++ stats.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", ()=>{
   let outDiv = ['out1', 'out2', 'out3', 'out4', 'out5'];
   let inDiv = ['in1', 'in2', 'in3', 'in4', 'in5'];
-  let percents = ['60', '80', '57', '83', '90']
+  let percents = ['60', '80', '57', '83', '70'];
+  let percentDiv = ['perc1', 'perc2', 'perc3', 'perc4', 'perc5'];
   for( let i = 0; i <  5; i++){
       let CircularBar = document.querySelector("." + inDiv[i]);
 
-      let PercentValue = document.querySelector(".percent");
+      // let PercentValue = document.querySelector(".percent");
+      let PercentValue = document.querySelector('.' + percentDiv[i]);
       
       let InitialValue = 0;
       let finaleValue = percents[i];
@@ -15,7 +17,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         InitialValue += 1;
       
         CircularBar.style.background = `conic-gradient(#16a6b6 ${InitialValue/100 * 360}deg, #fff 0deg)`;
-        PercentValue.innerHTML = InitialValue+"%";
+        PercentValue.innerHTML = InitialValue;
       
         if(InitialValue >= finaleValue){
           clearInterval(timer);
