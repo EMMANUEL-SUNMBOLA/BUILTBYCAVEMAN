39ca567d28fc8483a9769f22dd581d45a311cdf1
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-12 12:55:56 +0100

    floatbtn invisible

--- float.js
+++ float.js
@@ -1,4 +1,24 @@
 let topBtn = document.querySelector('.float');
-topBtn.addEventListener("click", ()=>{
-    window.scrollTo(0, 0);
-})
+
+// Function to check scroll position and toggle button visibility
+function toggleButtonVisibility() {
+    if (window.scrollY > 500) {
+        topBtn.style.display = 'flex'; // Show the button
+    } else {
+        topBtn.style.display = 'none'; // Hide the button
+    }
+}
+
+// Add event listener for the scroll event
+window.addEventListener('scroll', toggleButtonVisibility);
+
+// Initially hide the button when the page loads
+toggleButtonVisibility();
+
+// Scroll to top when the button is clicked
+topBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Optionally, add smooth scrolling behavior
+    });
+});
