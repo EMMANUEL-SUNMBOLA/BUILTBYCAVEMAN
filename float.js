49bfd80fdb99c04602fe3a098d7e63a8fb083943
ADE DAYO <adedayoemmanuel729@gmail.com>
2023-12-13 16:19:37 +0100

    buffalopartners done

--- float.js
+++ float.js
@@ -1,24 +1,17 @@
 let topBtn = document.querySelector('.float');
 
-// Function to check scroll position and toggle button visibility
 function toggleButtonVisibility() {
     if (window.scrollY > 500) {
-        topBtn.style.display = 'flex'; // Show the button
+        topBtn.style.display = 'flex';
     } else {
-        topBtn.style.display = 'none'; // Hide the button
+        topBtn.style.display = 'none';
     }
 }
-
-// Add event listener for the scroll event
 window.addEventListener('scroll', toggleButtonVisibility);
-
-// Initially hide the button when the page loads
 toggleButtonVisibility();
-
-// Scroll to top when the button is clicked
 topBtn.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // Optionally, add smooth scrolling behavior
+        behavior: 'smooth'
     });
 });
